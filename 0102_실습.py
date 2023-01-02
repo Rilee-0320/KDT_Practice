5bc09ba0d691fbdf6f7251bb89bd4aa1f1107cde
Rilee-0320 <go13209@naver.com>
2023-01-02 18:15:35 +0900

    23_01_02 python 실습예제

---
+++ 0102_실습.py
@@ -0,0 +1,70 @@
+# 실습 문제 1
+number = int(input('정수형 숫자를 입력해주세요 > '))
+print(number + 10)
+
+
+# 실습 문제 2
+food = input('좋아하는 음식을 입력해주세요 > ')
+print('좋아하는 음식 :', food)
+
+
+# 실습 문제 3
+name = input('이름을 입력해주세요 > ')
+year = 2023 - int(input('태어난 년도를 입력해주세요 > ')) + 1
+print('저의 이름은 ' + name + '이고, 올해 나이는 ' + str(year) + '세 입니다.')
+
+
+# 실습 문제 4
+string1 = input('첫 번째 문장을 입력해주세요 > ')
+string2 = input('두 번째 문장을 입력해주세요 > ')
+print(string1 + string2)
+
+
+# 실습 문제 5
+number = int(input('정수형 숫자를 입력해주세요 > '))
+print(number * -1)
+
+
+# 실습 문제 6
+number1 = int(input('첫 번째 정수형 숫자를 입력해주세요 > '))
+number2 = int(input('두 번째 정수형 숫자를 입력해주세요 > '))
+print('더하기 연산 :', number1 + number2)
+print('빼기 연산 :', number1 - number2)
+print('곱하기 연산 :', number1 * number2)
+print('몫 연산 :', number1 // number2)
+print('나머지 연산 :', number1 % number2)
+
+
+# 실습 문제 7
+number1 = int(input('첫 번째 정수형 숫자를 입력해주세요 > '))
+number2 = int(input('두 번째 정수형 숫자를 입력해주세요 > '))
+number3 = int(input('세 번째 정수형 숫자를 입력해주세요 > '))
+print(int((number1 + number2 + number3) / 3))
+
+
+# 실습 문제 8
+number1 = int(input('첫 번째 정수형 숫자를 입력해주세요 > '))
+number2 = int(input('두 번째 정수형 숫자를 입력해주세요 > '))
+number3 = int(input('세 번째 정수형 숫자를 입력해주세요 > '))
+number4 = int(input('네 번째 정수형 숫자를 입력해주세요 > '))
+number5 = int(input('다섯 번째 정수형 숫자를 입력해주세요 > '))
+print([number1, number2, number3, number4, number5])
+
+
+# 실습 문제 9
+string = input('문자열을 입력해주세요 > ')
+number = int(input('정수형 숫자를 입력해주세요 > '))
+print(string * number)
+
+
+# 실습 문제 10
+number1 = int(input('첫 번째 정수형 숫자를 입력해주세요 > '))
+print(number1)
+number2 = int(input('두 번째 정수형 숫자를 입력해주세요 > '))
+print(number1 + number2)
+number3 = int(input('세 번째 정수형 숫자를 입력해주세요 > '))
+print(number1 + number2 + number3)
+number4 = int(input('네 번째 정수형 숫자를 입력해주세요 > '))
+print(number1 + number2 + number3 + number4)
+number5 = int(input('다섯 번째 정수형 숫자를 입력해주세요 > '))
+print(number1 + number2 + number3 + number4 + number5)
