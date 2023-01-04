58c5800249ebb74d6d8284d24b688daa0674c91e
Rilee-0320 <go13209@naver.com>
2023-01-04 16:23:09 +0900

    23_01_04 python 실습예제

--- 0103_실습.py
+++ 0103_실습.py
@@ -1,4 +1,5 @@
 # 실습 문제 1
+# 정수 한 개를 입력받고 해당 숫자가 0보다 큰 숫자라면 True, 아니면 False를 출력하세요.
 num = int(input('정수를 입력하세요 > '))
 
 if num > 0:
@@ -8,6 +9,8 @@ else:
 
 
 # 실습 문제 2
+# 정수 두 개를 입력받고, 크기가 더 큰 정수를 출력하세요. 두 정수가 같으면 False를 출력하세요.
+# (1) 내 답안
 num1 = int(input('첫 번째 정수를 입력하세요 > '))
 num2 = int(input('두 번째 정수를 입력하세요 > '))
 
@@ -19,8 +22,21 @@ if num1 != num2:
 else:
     print(False)
 
+# (2) 모범 답안
+number1 = int(input("첫 번째 정수를 입력하세요 > "))
+number2 = int(input("두 번째 정수를 입력하세요 > "))
+
+if number1 == number2:
+    print(False)
+if number1 > number2:
+    print(number1)
+if number1 < number2:
+    print(number2)
+
 
 # 실습 문제 3
+# 정수 한 개를 입력받고, 해당 정수가 1보다 크고, 10보다 작다면 True 아니면 False를 출력하세요.
+# (1) 내 답안
 num = int(input('정수를 입력하세요 > '))
 
 if 1 < num < 10:
@@ -28,8 +44,17 @@ if 1 < num < 10:
 else:
     print(False)
 
+# (2) 모범 답안
+number = int(input("정수를 입력하세요 > "))
+
+if 1 < number and number < 10:
+    print(True)
+else:
+    print(False)
+
 
 # 실습 문제 4
+# 정수 한 개를 입력받고 0보다 크고, 짝수라면 True, 아니면 False를 출력하세요.
 num = int(input('정수를 입력하세요 > '))
 
 if num > 0:
@@ -42,6 +67,11 @@ else:
 
 
 # 실습 문제 5
+# 정수 한 개를 입력받고, 아래 조건에 따라 출력하세요.
+# 값이 100 초과 / 0 미만이면 "에러" 출력
+# 값이 60 이상이면 "합격" 출력
+# 값이 60 미만이면 "불합격" 출력
+# (1) 내 답안
 num = int(input('정수를 입력하세요 > '))
 
 if 0 <= num <= 100:
@@ -52,8 +82,21 @@ if 0 <= num <= 100:
 else:
     print('에러')
 
+# (2) 모범 답안
+number = int(input("정수를 입력하세요 > "))
+
+if number > 100 or number < 0:
+    print("에러")
+else:
+    if number >= 60:
+        print("합격")
+    else:
+        print("불합격")
+
 
 # 실습 문제 6
+# 문자열을 입력받고, 입력받은 문자열을 반대로 한 글자씩 출력하세요.
+# 힌트 : 문자열 역슬라이싱
 string = 'hello world'
 
 for char in string[::-1]:
@@ -61,6 +104,7 @@ for char in string[::-1]:
 
 
 # 실습 문제 7
+# 정수 두 개를 입력받고, 두 수 사이의 정수를 오름차순으로 출력하세요. 두 값이 같으면 False를 출력하세요.
 num1 = int(input('첫 번째 정수를 입력하세요 > '))
 num2 = int(input('두 번째 정수를 입력하세요 > '))
 
@@ -75,6 +119,7 @@ else:
 
 
 # 실습 문제 8
+# 정수 두 개를 입력받고, 두 수 사이의 정수를 내림차순으로 한 줄에 모두 출력하세요. 두 값이 같으면 False를 출력하세요.
 num1 = int(input('첫 번째 정수를 입력하세요 > '))
 num2 = int(input('두 번째 정수를 입력하세요 > '))
 
@@ -89,6 +134,8 @@ else:
 
 
 # 실습 문제 9
+# 정수 한 개를 입력받고, 1부터 입력값 사이의 정수 중 홀수만 출력하세요. 입력값이 1보다 작으면 False를 출력하세요.
+# (1) 내 답안
 num = int(input('정수를 입력하세요 > '))
 if  num < 1:
     print(False)
@@ -96,8 +143,19 @@ else:
     for i in range(1, num, 2):
         print(i)
 
+# (2) 모범 답안
+number = int(input("정수를 입력하세요 > "))
+
+if number < 1:
+    print(False)
+else:
+    for element in range(1, number):
+        if element % 2 == 1:
+            print(element)
+
 
 # 실습 문제 10
+# 구구단을 출력하세요.
 for n1 in range(2, 10):
     for n2 in range(2, 10):
         print(f'{n1} X {n2} = {n1*n2}')
---
+++ 0104_실습.py
@@ -0,0 +1,118 @@
+# 실습 문제 1
+# 정수 한 개를 입력받고, 해당 정수의 절대값을 출력하세요. 단, abs() 함수는 사용하지 마세요.
+num = int(input('정수를 입력하세요 > '))
+
+if num >= 0:
+    print(num)
+else:
+    print(num * -1)
+
+
+# 실습 문제 2
+# 정수만 저장한 리스트가 주어집니다. 리스트 요소의 개수를 출력하세요. 단, len() 함수는 사용하지 마세요.
+number_list = [1, 2, 3, 4, 5]
+cnt = 0
+
+for i in number_list:
+    cnt += 1
+print(cnt)
+
+number_list = []
+cnt = 0
+
+for i in number_list:
+    cnt += 1
+print(cnt)
+
+
+# 실습 문제 3
+# 정수만 저장한 리스트가 주어집니다. 리스트에 저장된 정수들의 합을 출력하세요. 단, sum() 함수는 사용하지 마세요.
+number_list = [1, 2, 3, 4, 5]
+total = 0
+
+for i in number_list:
+    total += i
+print(total)
+
+number_list = [-1, -2, -3, -4, -5]
+total = 0
+
+for i in number_list:
+    total += i
+print(total)
+
+
+# 실습 문제 4
+# 정수만 저장한 리스트가 주어집니다. 리스트에 저장된 정수들의 평균을 출력하세요. 단, len() / sum() 함수는 사용하지 마세요.
+number_list = [2, 4, 6]
+total = 0
+cnt = 0
+
+for i in number_list:
+    total += i
+    cnt += 1
+print(total/cnt)
+
+number_list = [2, 3, 5, 7]
+total = 0
+cnt = 0
+
+for i in number_list:
+    total += i
+    cnt += 1
+print(total/cnt)
+
+
+# 실습 문제 5
+# 정수만 저장한 리스트가 주어집니다. 리스트에 저장된 정수들의 곱을 출력하세요.
+number_list = [1, 2, 3, 4, 5]
+total = 1
+
+for i in number_list:
+    total *= i
+print(total)
+
+number_list = [-1, -2, 3]
+total = 1
+
+for i in number_list:
+    total *= i
+print(total)
+
+
+# 실습 문제 6
+# 양의 정수만 저장한 리스트가 주어집니다. 리스트에 저장된 정수 중 가장 큰 값을 출력하세요. 단, max() 함수는 사용하지 마세요.
+number_list = [1, 2, 3, 4, 5]
+x = number_list[0]
+
+for i in number_list:
+    if i > x:
+        x = i
+print(x)
+
+number_list = [1, 1, 1]
+x = number_list[0]
+
+for i in number_list:
+    if i > x:
+        x = i
+print(x)
+
+
+# 실습 문제 7
+# 양의 정수만 저장한 리스트가 주어집니다. 리스트에 저장된 정수 중 가장 작은 값을 출력하세요. 단, min() 함수는 사용하지 마세요.
+number_list = [1, 2, 3, 4, 5]
+x = number_list[0]
+
+for i in number_list:
+    if i < x:
+        x = i
+print(x)
+
+number_list = [5, 5, 5, 2]
+x = number_list[0]
+
+for i in number_list:
+    if i < x:
+        x = i
+print(x)
