3e3906a8aa4f446c515cb794105beca5856a02cd
thebjko <thebjko@gmail.com>
2023-04-13 16:29:23 +0900

    require email 기능 구현

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,16 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 
 class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2',)
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'password1',
+            'password2',
+        )
