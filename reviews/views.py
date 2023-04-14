03fed03e99eafb62e3627b9f6489480d56412ce3
doll2gom <shkim_ok@naver.com>
2023-04-14 10:00:37 +0900

    start reviews/create

--- reviews/views.py
+++ reviews/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'reviews/index.html')
+
+def create(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = 
+    return 
