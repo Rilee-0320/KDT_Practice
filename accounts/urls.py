58e4bc03412fb0b2c0b817e5b840d9fbeafc360b
thebjko <thebjko@gmail.com>
2023-04-14 16:44:10 +0900

    profile 완료

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('update/', views.update, name='update'),
     path('delete/', views.delete, name='delete'),
+    path('profile/<int:user_pk>', views.profile, name='profile'),
 ]
