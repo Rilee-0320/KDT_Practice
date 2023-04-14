8bc4a0859f30524faa9f284484e8419a6daa54bc
Rilee-0320 <go13209@naver.com>
2023-04-14 11:20:31 +0900

    reviews create 기능 구현

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Review
+from .forms import ReviewForm
 
 # Create your views here.
 def index(request):
@@ -10,10 +12,19 @@ def index(request):
     return render(request, 'reviews/index.html', context)
 
 
+@login_required
 def create(request):
     if request.method == 'POST':
-        pass
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.user = request.user
+            review.save()
+            return redirect('reviews:index')
     else:
-        form = 
-    return 
+        form = ReviewForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'reviews/create.html', context)
     
