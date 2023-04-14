3e52cd197d19300b644381d32147f11f87bed8b6
thebjko <thebjko@gmail.com>
2023-04-14 16:20:42 +0900

    review delete 구현

--- reviews/urls.py
+++ reviews/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('create/', views.create, name='create'),
     path('detail/<int:review_pk>', views.detail, name='detail'),
     path('update/<int:review_pk>/', views.update, name='update'),
+    path('delete/<int:review_pk>/', views.delete, name='delete'),
     path('detail/<int:review_pk>/comment_create/', views.comment_create, name='comment_create'),
     path('detail/<int:review_pk>/comment_delete/<int:comment_pk>/', views.comment_delete, name='comment_delete'),
 ]
