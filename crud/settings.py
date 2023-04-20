9eba6ae3eb5691c257df2eab0d3ff365f19f170a
DreamofHeaven <eunjeong0223@naver.com>
2023-04-20 15:41:27 +0900

    미디어 파일 이름 수정

--- crud/settings.py
+++ crud/settings.py
@@ -126,9 +126,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'img'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/img/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
