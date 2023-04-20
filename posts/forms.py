e0ad96f90d2d9562ab51174e661cd02aa9d70534
Seongjoo Lim <afro1662@naver.com>
2023-04-20 12:41:04 +0900

    Add css

--- posts/forms.py
+++ posts/forms.py
@@ -5,3 +5,11 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'select1_content', 'select2_content',)
+        label_suffix = ''
+    
+    title = forms.CharField(label='주제', label_suffix='', widget=forms.TextInput(
+        attrs={'class': 'form-control', 'style': 'width: 200px;'}))
+    select1_content = forms.CharField(label='왼쪽', label_suffix='', widget=forms.TextInput(
+        attrs={'class': 'form-control', 'style': 'width: 200px;'}))
+    select2_content = forms.CharField(label='오른쪽', label_suffix='', widget=forms.TextInput(
+        attrs={'class': 'form-control', 'style': 'width: 200px;'}))
